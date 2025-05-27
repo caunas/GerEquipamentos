9eab0df037d732765dd0a9d7d932ae863120b7e2
Cauan Nascimento <caunas1337@proton.me>
2025-05-27 11:38:52 -0300

    Implementação de verificadores nos utilitários do projeto

--- src/util/Verificador.java
+++ src/util/Verificador.java
@@ -1,4 +1,16 @@
 package util;
 
-public class verificador {
+/**
+* Autor: Vinicius
+**/
+
+public class Verificador {
+    public static boolean validarNome(String nome) {
+        return nome != null && nome.trim().length() > 1;
+    }
+    public static boolean validarCategoria(String categoria) {
+        return categoria != null && categoria.equalsIgnoreCase("Construção") ||
+                categoria.equalsIgnoreCase("Manutenção") ||
+                categoria.equalsIgnoreCase("Informática");
+    }
 }

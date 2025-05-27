37de9e4962085c7ad8e24211c50eb46f1219f1ce
Cauan Nascimento <caunas1337@proton.me>
2025-05-27 11:37:52 -0300

    Atualização de Interface de linha de comando

--- src/ui/CLI.java
+++ src/ui/CLI.java
@@ -1,33 +1,61 @@
+/**
+ *Autor: Vinicius
+ * A classe CLI é responsável pela interface de linha de comando
+ * Essa classe deve ser depreciada com a implementação da interface gráfica
+ **/
 package ui;
 import java.util.Scanner;
 import core.EquipamentoDAO;
 
 public class CLI{
+  // Gerenciador de equipamentos
   EquipamentoDAO object = new EquipamentoDAO();
-  public static void show_menu(){
-    System.out.println("Selecione uma opção");
-    System.out.println("=========================================");
-    System.out.println("1 - EquipamentoDAO");
-    System.out.println("=========================================");
-    System.out.println("0 - Sair");
-  }
 
-  public static void catch_option(int escolha){
-    switch(escolha){
-      case 0:
-        System.out.println("Encerrando...");
-        System.exit(0);
-      case 1:
-        EquipamentoDAO.teste();
-        break;
-      default:
-        System.out.println("Opção inválida.");
+    public static void call() {
+      Scanner scanner = new Scanner(System.in);
+
+      boolean saida = false;
+
+      do {
+        switch(menu(scanner)) {
+          case 1:
+            EquipamentoDAO.teste(); // Cadastrar equipamento, chamar func cadastrar do EquipamentoDAO
+            break;
+          case 5:
+            saida = true;
+            System.out.println("\nSaindo...");
+            break;
+        }
+      }while(!saida);
+
+    }
+
+    public static int menu(Scanner scanner) {
+
+      System.out.println("\n****************************");
+      System.out.println("| CADASTRO DE EQUIPAMENTOS |");
+      System.out.println("****************************\n");
+
+      System.out.println("Tipos disponíveis de categorias para cadastro:\n");
+      System.out.println(".Manutenção");
+      System.out.println(".Construção");
+      System.out.println(".Informática");
+
+
+      System.out.println("\n");
+      System.out.println("**************************");
+      System.out.println("*          Menu          *");
+      System.out.println("**************************");
+      System.out.println("*   1. Cadastrar         *");
+      System.out.println("*   2. Exibir cadastro   *");
+      System.out.println("*   3. Editar cadastro   *");
+      System.out.println("*   4. Excluir cadastro  *");
+      System.out.println("*   5. Sair              *");
+      System.out.println("**************************");
+
+      System.out.println("\nEscolha uma opção:");
+      int opcao = scanner.nextInt();
+
+      return opcao;
     }
   }
-
-  public static void call(){
-    Scanner scan = new Scanner(System.in);
-    show_menu();
-    catch_option(scan.nextInt());
-  }
-}

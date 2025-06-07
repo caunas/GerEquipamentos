e98a58966f1b761484d37dfaee5c288a30089de6
Mariana <164277163+m4rimoreira@users.noreply.github.com>
2025-06-07 16:34:19 -0300

    Classe Equipamento integrada ao JavaFX

--- src/core/Equipamento.java
+++ src/core/Equipamento.java
@@ -5,51 +5,36 @@ Autores:
 */
 
 package core;
-import java.util.Date;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Equipamento {
-    private int id;
-    private String nome;
-    private String categoria;
-    private Date dataCadastro;
-    private String detalhes;
-      public Equipamento(int id, String nome, String categoria, String detalhes){
-         this.id = id;
-         this.nome = nome;
-         this.categoria = categoria;
-         this.dataCadastro = new Date();
-         this.detalhes = detalhes;
-      }
-        public String getNome(){
-          return nome;
-        }  
-         public void setNome(String nome){
-          this.nome = nome;
-         }
-         public String getCategoria(){
-          return categoria;
+  
+   // Propriedades  para o nome, id e tipo (permitem binding e listeners)
+    private final IntegerProperty id;
+    private final StringProperty nome;
+    private final StringProperty tipo;
 
-         }
-         public void setCategoria(String categoria){
-          this.categoria = categoria;
+    public Equipamento(int id, String nome, String tipo) {
+      
+      // Inicializam as propriedades com o valor recebido
+        this.id = new SimpleIntegerProperty(id);
+        this.nome = new SimpleStringProperty(nome);
+        this.tipo = new SimpleStringProperty(tipo);
+    }
 
-         }
-         public int getId(){return id;}
-         public String getDetalhes(){
-          return detalhes;
-         }
-         public void setDetalhes(String detalhes){
-          this.detalhes = detalhes;
-         }
-         public Date getDataCadastro(){
-          return dataCadastro;
-         }
-         public void setDataCadastro(Date dataCadastro){
-          this.dataCadastro = dataCadastro;
-  }
-  @Override
-  public String toString(){
-    return "Equipamento [ID= " + id +", Nome = " + nome + ", Categoria = " + categoria +
-     ", Data de Cadastro = " + dataCadastro + ", Detalhes = " + detalhes + "]";
-  }
-}      
+    // Getters e Setters
+    public int getId() { return id.get(); }
+    public void setId(int id) { this.id.set(id); }
+    public IntegerProperty idProperty() { return id; }
+
+    public String getNome() { return nome.get(); }
+    public void setNome(String nome) { this.nome.set(nome); }
+    public StringProperty nomeProperty() { return nome; }
+
+    public String getTipo() { return tipo.get(); }
+    public void setTipo(String tipo) { this.tipo.set(tipo); }
+    public StringProperty tipoProperty() { return tipo; }
+}   

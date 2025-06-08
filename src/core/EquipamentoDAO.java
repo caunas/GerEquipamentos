b631b902b780fd75e9363d932aa6a97c5a74a424
Robert Filipe Miranda Costa <163329263+Lipe22md@users.noreply.github.com>
2025-06-08 00:48:01 -0300

    Update EquipamentoDAO.java

--- src/core/EquipamentoDAO.java
+++ src/core/EquipamentoDAO.java
@@ -1,91 +1,84 @@
 package core;
-import java.util.List;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Autor: Robert
  * Essa classe é responsável por gerênciar a lista de equipamentos
+ * Jeremias 29:11
  * */
 
 public class EquipamentoDAO {
-  static List<Equipamento> EquipamentoLista = new ArrayList<>();
-  private static int indiceProximo = 1;
+    private static List<Equipamento> equipamentoLista = new ArrayList<>();
+    private static int indiceProximo = 1;
 
-  public static List listarTodos(){
-    return EquipamentoLista;
-  }
-
-  public static List buscarPorNome(String nome) {
-    List<String> out = new ArrayList<>();
-    for (int i = 0; i < EquipamentoLista.size(); i++) {
-      if (EquipamentoLista.get(i).getNome() == nome) {
-        out.add(EquipamentoLista.get(i).toString());
-      }
+    public static List<Equipamento> listarTodos() {
+        return equipamentoLista;
     }
-    return out;
-  }
 
-  public static String buscarPorId(int id){
-    String out = "";
-    for(int i = 0; i < EquipamentoLista.size(); i++){
-      if(EquipamentoLista.get(i).getId() == id){
-        out = EquipamentoLista.get(i).toString();
-      } else{
-        out = "n tem n, rs";
-      }
-    }
-    return out;
-  }
-
-  public static List buscarPorCategoria(String categoria){
-    List<String> out = new ArrayList<>();
-    for (int i = 0; i < EquipamentoLista.size(); i++) {
-      if (EquipamentoLista.get(i).getCategoria() == categoria) {
-        out.add(EquipamentoLista.get(i).toString());
-      }
-    }
-    return out;
-  }
-
-  public static void atualizar(int id, String filtro, String novo_dado){
-    for(int i = 0; i < EquipamentoLista.size(); i++){
-      if(EquipamentoLista.get(i).getId() == id){
-        if(filtro == "nome"){
-          // atualizar nome
-          EquipamentoLista.get(i).setNome(novo_dado);
-        } else if (filtro == "categoria") {
-          // atualizar categoria
-          EquipamentoLista.get(i).setDetalhes(novo_dado);
-        } else if(filtro == "detalhes"){
-          // atualizar detalhes
-          EquipamentoLista.get(i).setDetalhes(novo_dado);
+    public static List<Equipamento> buscarPorNome(String nome) {
+        List<Equipamento> out = new ArrayList<>();
+        for (Equipamento eq : equipamentoLista) {
+            if (eq.getNome().equalsIgnoreCase(nome)) {
+                out.add(eq);
+            }
         }
-      }
+        return out;
     }
-  }
 
-  public static void adicionar(String nome, String categoria, String detalhes) {
-    Equipamento novoEquip = new Equipamento(indiceProximo,
-            nome,
-            categoria,
-            detalhes);
-
-    EquipamentoLista.add(novoEquip);
-
-    indiceProximo++;
-  }
-
-  public static void remover(int id){
-    for(int i = 0; i <= EquipamentoLista.size(); i++){
-      if(EquipamentoLista.get(i).getId() == id){
-        EquipamentoLista.remove(i);
-      }
+    public static Equipamento buscarPorId(int id) {
+        for (Equipamento eq : equipamentoLista) {
+            if (eq.getId() == id) {
+                return eq;
+            }
+        }
+        return null;
     }
-  }
 
-  @Override
-  public String toString(){
-    return "Data Acess Object is Online!";
-  }
+    public static List<Equipamento> buscarPorCategoria(String categoria) {
+        List<Equipamento> out = new ArrayList<>();
+        for (Equipamento eq : equipamentoLista) {
+            if (eq.getCategoria().equalsIgnoreCase(categoria)) {
+                out.add(eq);
+            }
+        }
+        return out;
+    }
 
+    public static void atualizar(int id, String filtro, String novoDado) {
+        Equipamento eq = buscarPorId(id);
+        if (eq != null) {
+            switch (filtro.toLowerCase()) {
+                case "nome":
+                    eq.setNome(novoDado);
+                    break;
+                case "categoria":
+                    eq.setCategoria(novoDado);
+                    break;
+                case "detalhes":
+                    eq.setDetalhes(novoDado);
+                    break;
+            }
+        }
+    }
+
+    public static void adicionar(String nome, String categoria, String detalhes) {
+        Equipamento novoEquip = new Equipamento(indiceProximo++, nome, categoria, detalhes);
+        equipamentoLista.add(novoEquip);
+    }
+
+    public static boolean remover(int id) {
+        return equipamentoLista.removeIf(eq -> eq.getId() == id);
+    }
+
+    public static void limparTudo() {
+        equipamentoLista.clear();
+        indiceProximo = 1;
+    }
+
+    @Override
+    public String toString() {
+        return "DAO operacional com " + equipamentoLista.size() + " equipamentos.";
+    }
 }

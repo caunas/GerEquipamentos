52db54b7fb201fa9f81ac7457680a02da43b31d0
Cauan Nascimento <caunas1337@proton.me>
2025-06-18 05:32:46 -0300

    ADD: Recursos de interface para: Cadastro, Consulta e Busca

--- src/ui/GUI/EquipamentoControl.java
+++ src/ui/GUI/EquipamentoControl.java
@@ -2,8 +2,8 @@ package ui.GUI;
 
 /**
  * Jeremias 29:11
- * Autor: Robert
- * */
+ * Autores: Robert, Cauan
+ **/
 
 import core.Equipamento;
 import core.EquipamentoDAO;
@@ -12,26 +12,53 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 public class EquipamentoControl {
 
+    // campos de formulario
     @FXML private TextField nomeField;
-    @FXML private TextField tipoField;
     @FXML private TextField buscaField;
+    @FXML private ChoiceBox<String> categoriaSelector;
+    @FXML private ChoiceBox<String> categoriaSelectorBusca;
+    @FXML private TextField detalhesField;
+
+    // tabela e colunas
     @FXML private TableView<Equipamento> tabela;
     @FXML private TableColumn<Equipamento, Integer> colId;
     @FXML private TableColumn<Equipamento, String> colNome;
-    @FXML private TableColumn<Equipamento, String> colTipo;
+    @FXML private TableColumn<Equipamento, String> colCategoria;
+    @FXML private TableColumn<Equipamento, String> colData;
+    @FXML private TableColumn<Equipamento, String> colDetalhes;
+
+    // labels
     @FXML private Label totalLabel;
 
+
     private ObservableList<Equipamento> equipamentos = FXCollections.observableArrayList();
+    private ObservableList<String> categorias_disponiveis = FXCollections.observableArrayList(
+            "Construção",
+            "Manutenção",
+            "Informática"
+    );
 
     @FXML
     public void initialize() {
         // Inicializa as colunas da tabela com os dados do Equipamento
         colId.setCellValueFactory(cell -> new javafx.beans.property.SimpleIntegerProperty(cell.getValue().getId()).asObject());
         colNome.setCellValueFactory(cell -> new javafx.beans.property.SimpleStringProperty(cell.getValue().getNome()));
-        colTipo.setCellValueFactory(cell -> new javafx.beans.property.SimpleStringProperty(cell.getValue().getCategoria()));
+        colCategoria.setCellValueFactory(cell -> new javafx.beans.property.SimpleStringProperty(cell.getValue().getCategoria()));
+        colData.setCellValueFactory(cell -> new javafx.beans.property.SimpleStringProperty(cell.getValue().getDataCadastro()));
+        colDetalhes.setCellValueFactory(cell -> new javafx.beans.property.SimpleStringProperty(cell.getValue().getDetalhes()));
+
+        // Inicializa as opções de seleção das categorias
+        categoriaSelector.setItems(categorias_disponiveis);
+        categoriaSelectorBusca.setItems(categorias_disponiveis);
 
         atualizarTabela();
     }
@@ -39,11 +66,11 @@ public class EquipamentoControl {
     @FXML
     public void adicionarEquipamento(ActionEvent event) {
         String nome = nomeField.getText();
-        String tipo = tipoField.getText();
-        String detalhes = "N/A"; // Pode ajustar depois
+        String categoria = categoriaSelector.getValue();
+        String detalhes = detalhesField.getText(); // Ajustado :)
 
-        if (!nome.isEmpty() && !tipo.isEmpty()) {
-            EquipamentoDAO.adicionar(nome, tipo, detalhes);
+        if (!nome.isEmpty() && !categoria.isEmpty()) {
+            EquipamentoDAO.adicionar(nome, categoria, detalhes);
             atualizarTabela();
             limparCampos(null);
         } else {
@@ -56,16 +83,18 @@ public class EquipamentoControl {
         String busca = buscaField.getText();
 
         try {
-            int id = Integer.parseInt(busca);
+            int id = parseInt(busca);
             String novoNome = nomeField.getText();
-            String novoTipo = tipoField.getText();
+            //String novoTipo = tipoField.getText();
 
             if (!novoNome.isEmpty()) {
                 EquipamentoDAO.atualizar(id, "nome", novoNome);
             }
+            /*
             if (!novoTipo.isEmpty()) {
                 EquipamentoDAO.atualizar(id, "categoria", novoTipo);
             }
+             */
 
             atualizarTabela();
             limparCampos(null);
@@ -75,10 +104,41 @@ public class EquipamentoControl {
     }
 
     @FXML
+    public void buscaPorCategoria(ActionEvent event) {
+        String busca = categoriaSelectorBusca.getValue();
+        try{
+            List buffer = EquipamentoDAO.buscarPorCategoria(busca);
+            equipamentos.setAll(buffer);
+            totalLabel.setText(equipamentos.size() + " equipamentos");
+        } catch(Exception e){
+            System.out.println(e);
+            alert(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void buscaPorId(ActionEvent event){
+        String busca = buscaField.getText();
+        try{
+            Equipamento buffer = EquipamentoDAO.buscarPorId(parseInt(busca));
+            equipamentos.setAll(buffer);
+            totalLabel.setText(equipamentos.size() + " equipamentos");
+        } catch(Exception NumberFormatException){
+            alert("Insira um ID válido!");
+        }
+    }
+
+    @FXML
+    public void buscaPorNome(ActionEvent event) {
+        nomeField.clear();
+        buscaField.clear();
+    }
+
+    @FXML
     public void removerEquipamento(ActionEvent event) {
         String busca = buscaField.getText();
         try {
-            int id = Integer.parseInt(busca);
+            int id = parseInt(busca);
             EquipamentoDAO.remover(id);
             atualizarTabela();
             limparCampos(null);
@@ -90,10 +150,10 @@ public class EquipamentoControl {
     @FXML
     public void limparCampos(ActionEvent event) {
         nomeField.clear();
-        tipoField.clear();
         buscaField.clear();
     }
 
+    @FXML
     private void atualizarTabela() {
         equipamentos.setAll(EquipamentoDAO.listarTodos());
         tabela.setItems(equipamentos);
